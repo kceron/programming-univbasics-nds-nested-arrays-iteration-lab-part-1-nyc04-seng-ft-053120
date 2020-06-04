0777c4b4e40b6232dea1c7b181a43a44b02eab92
Karem Ceron <karemiceron@gmail.com>
2020-06-04 02:28:31 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -14,3 +14,20 @@ def find_even_values(src)
       row_index += 1 
     end 
 end
+=begin
+spice_rack = [
+  ["Posh", "Scary", "Sporty"],
+  ["Paprika", "Fajita Mix", "Coriander"],
+  ["Parsley", "Sage", "Rosemary"]
+]
+ 
+row_index = 0
+while row_index < spice_rack.count do
+  element_index = 0
+  while element_index < spice_rack[row_index].count do
+    p spice_rack[row_index][element_index]
+    element_index += 1
+  end
+  row_index += 1
+end
+=end 
